121b655a7468d1518b50e1b0fb01f2154577c733
Batuhan Güneş <batuhan.gunes@pointrlabs.com>
2020-04-29 02:23:02 +0300

    add project

---
+++ signLanguageConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  signLanguageConverter
+//
+//  Created by Batuhan Güneş on 9.11.2019.
+//  Copyright © 2019 Batuhan Güneş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
